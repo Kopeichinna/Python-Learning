68220242fb83fbe21a2b9c4a325fe9a73f98dbf8
Inna Kopeichykova <inna@Innas-MacBook.local>
2023-06-19 13:16:33 +0100

    Added home task 4

--- Task4.py
+++ Task4.py
@@ -1,8 +1,11 @@
-# we have four values w,x,y,z
-# write if-elif-else statement that will search minimum value and print smth aka "'y' is minimum value'
-# advice use python debugger and different values to check your algorithm
+"""
+Home task 4. If- statements.
 
-"""Minimum value search out of 4 values."""
+Task description:
+we have four values w,x,y,z
+write if-elif-else statement that will search minimum value and print smth aka "'y' is minimum value'
+advice use python debugger and different values to check your algorithm
+  """
 
 w, x, y, z = 100, 200, 40, 300
 

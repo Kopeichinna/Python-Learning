5cfb118558d44283dfe865007a9f2eba36cf565d
Kopeichinna <123162034+Kopeichinna@users.noreply.github.com>
2023-06-21 14:19:29 +0100

    Branch1 (#3)
    * Adding task 2
    
    * added Task 3
    
    * hopefully with added files this time;)
    
    * added some changes
    
    * adding Task 4
    
    * Added home task 4
    
    * Adding Task 5
    
    * deleting task5
    
    ---------
    
    Co-authored-by: Inna Kopeichykova <inna@Innas-MacBook.local>

--- Task4.py
+++ Task4.py
@@ -1,8 +1,14 @@
-# we have four values w,x,y,z
-# write if-elif-else statement that will search minimum value and print smth aka "'y' is minimum value'
-# advice use python debugger and different values to check your algorithm
+branch1
+"""
+Home task 4. If- statements.
 
-"""Minimum value search out of 4 values."""
+Task description:
+we have four values w,x,y,z
+write if-elif-else statement that will search minimum value and print smth aka "'y' is minimum value'
+advice use python debugger and different values to check your algorithm
+  """
+
+ main
 
 w, x, y, z = 100, 200, 40, 300
 

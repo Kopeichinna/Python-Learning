ebd947b3195a10210d7799d39f9d5b96eae94f6e
Kopeichinna <123162034+Kopeichinna@users.noreply.github.com>
2023-07-26 14:04:24 +0100

    Added Task 10 (#10)
    * Added Task 10
    
    * Added filter for the 4-th upper case letter
    
    ---------
    
    Co-authored-by: Inna Kopeichykova <inna@Innas-MacBook.local>

---
+++ Task_10.py
@@ -0,0 +1,28 @@
+"""
+    Task 10: Regex.
+
+    Description:
+
+open input.txt file and find all small english letters
+that match such conditions:
+target small letter round exactly with three capital english letters
+on the left and on the right
+Match examples:
+sdTRYaUVKn  -> matches "a"
+NTRSjARTb   -> no match ( not exactly 3 capital letters on the left)
+zDFGbOPNq   -> matches "b"
+Print Output as : "Result: "<your_found_human_readable_string">
+"""
+
+import re
+
+try:
+    with open('input1.txt', 'r') as fh:
+        input_data = fh.read()
+        # getting the list of letters matching the search criteria
+        letters_found = re.findall('[^A-Z][A-Z]{3}([a-z])[A-Z]{3}[^A-Z]',
+                                   string=input_data)
+        print(f'"Result: {letters_found}')
+
+except Exception as g_exc:
+    print(g_exc)
